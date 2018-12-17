6617e0b3f51ea46957c9d022c009c718d2b414d5
Victor Smirnov <aist11@gmail.com>
2018-12-16 20:40:56 -0500

    Limiting libbacktrace build Linux only for now

--- ports/libbacktrace/portfile.cmake
+++ ports/libbacktrace/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 
 if(NOT (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux"))
-    message(FATAL_ERROR "${PORT} currently support Linux only")
+    message(FATAL_ERROR "${PORT} currently supports Linux only")
 endif()
 
 
